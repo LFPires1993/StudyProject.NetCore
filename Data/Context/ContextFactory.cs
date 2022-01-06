71f1831ef40755a82d4012305e0287969012d092
lucas.pires <lfpires1993@gmail.com>
2022-01-06 09:56:05 -0300

    Create

---
+++ Data/Context/ContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var conectionString = "Server=localhost;Port=3306;Database=dbApi;Uid=root;Pwd=Lipe-1993";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(conectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
